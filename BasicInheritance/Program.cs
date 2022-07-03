c18763dd3f6343fac0dedb08dbf2b33ec9eadcbf
Covali Andrei <krab4ek@gmail.com>
2022-07-03 12:16:50 +0300

    Add project files.

---
+++ BasicInheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿using BasicInheritance;
+
+Console.WriteLine($"****** Basic Inheritance ******\n");
+
+Car myCar = new Car(80);
+Console.WriteLine($"My car is going {myCar.Speed}");
+
+myCar.Speed = 50;
+Console.WriteLine($"My car is going {myCar.Speed}");
+
+
+Console.ReadLine();
